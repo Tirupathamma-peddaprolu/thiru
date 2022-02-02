b2f22e7cd264f77dd6057347773e1260b4ad2feb
Tirupathamma-peddaprolu <98864770+Tirupathamma-peddaprolu@users.noreply.github.com>
2022-02-02 11:25:00 +0530

    Add files via upload

---
+++ bookmydr/src/main/java/com/spring/bookmydr/BookmydrApplication.java
@@ -0,0 +1,13 @@
+package com.spring.bookmydr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmydrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmydrApplication.class, args);
+	}
+
+}
